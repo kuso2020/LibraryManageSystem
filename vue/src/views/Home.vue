05e71a5cfa10cb76ba07497e11b01f427dff24da
kuso2020 <cymming3@outlook.com>
2025-01-08 15:27:54 +0800

    initVersion

---
+++ vue/src/views/Home.vue
@@ -0,0 +1,57 @@
+<template>
+
+  <div>
+    <div style="margin-bottom: 20px;">
+        <el-input v-model="data.input" placeholder="please input" size="normal" clearable style="width: 300px;" :prefix-icon="Search"></el-input>
+        <!-- test -->
+        <el-input v-model="data.input2" placeholder="要在输入框中添加图标，你可以简单地使用 prefix-icon 和 suffix-icon 属性。 另外， prefix 和 suffix 命名的插槽也能正常工作。s要在输入框中添加图标，你可以简单地使用 prefix-icon 和 suffix-icon 属性。 另外， prefix 和 suffix 命名的插槽也能正常工作。Us" size="normal" clearable @change="" style="width: 400px;" type="textarea" ></el-input>
+        
+    </div>
+
+    <div style="margin-bottom: 30px;">
+        <el-select
+      v-model="data.value"
+      placeholder="Select"
+      size="large"
+      style="width: 240px"
+    >
+      <el-option
+        v-for="item in options"
+        :key="item.value"
+        :label="item.label"
+        :value="item.value"
+      />
+    </el-select>
+        
+
+    </div>
+
+
+    <div>
+        just a test for git
+    </div>
+
+
+  </div>
+
+
+</template>
+
+
+<script setup>
+import { ref, reactive } from 'vue'
+import { Search } from '@element-plus/icons-vue';
+const data = reactive({
+  input: null,
+  input2: null,
+  value: null,
+})
+
+
+const options = [
+  { value: 'Option1', label: 'Option1' },
+  { value: 'Option2', label: 'Option2' },
+  { value: 'Option3', label: 'Option3' },
+]
+
+</script>
---
+++ vue/src/views/test.vue
@@ -0,0 +1,185 @@
+
+<template>
+    <div>
+      <div style="background-color: red; font-size: 25px; font-weight : bolder;color : aqua; text-decoration: underline;">
+          你好，欢迎使用图书管理系统！
+  
+      </div>
+      
+      <div style="font-size: 35px; color: blue;">
+          {{ a }}
+  
+          {{ data.name }} 的年龄是 {{ data.age }}，性别是 {{ data.sex }}。
+      </div>
+  
+      <!-- 数据的绑定 -->
+      <div>
+          <input v-model="data.name"/>
+      </div>
+  
+  
+      <div style="margin-bottom: 15px;">
+          <!-- 当 data.name 等于 '红猪' 时，显示 '小红猪'，否则显示 '小懒猪' -->
+          <span style="color: red;" v-if="data.name === '红猪'"> 小红猪 </span>
+          <span style="color: blue;" v-if="data.name === '懒猪'"> 小懒猪 </span>
+          <span style="color: green;" v-else> 不是红也不是懒猪 </span>
+  
+      </div>
+  
+      <div style="display: flex;margin-bottom: 30px;">
+          <!-- @click和v-for一起使用 -->
+          <div @click="show(item)" v-for="item in data.arr" style="height: 200px;width: 200px; background-color: yellow; margin-bottom: 15px; text-align: center;
+              line-height: 200px; font-size: 30px;margin: 15px;" >{{ item }}</div>
+  
+  
+          <div>
+              <select style="width: 300px;">
+                  <!-- 用for来将选项中的所有元素放入x-option标签中 -->
+                  <option v-for="fruit in data.fruits" >{{ fruit }} </option>
+              </select>
+          </div>
+  
+      </div>
+  
+      <div style="color: hotpink; font-size: 20px; margin-bottom: 10px;
+      background-color: bisque;text-align: center;height: 300px;width: 300px;line-height: 300px;">
+          {{ data.cnt }}
+      </div>
+  
+      <div>
+          <!-- submit按钮的点击事件 -->
+           <!-- click为自定义函数 -->
+          <button @click="click">提交</button>
+      </div>
+  
+      <div :style="data.box">test</div>
+      
+      <!-- 动态引用图片    -->
+      <div style="margin-bottom: 30px;">
+          <img :src="data.img" alt="">
+      </div>
+  
+      <!-- el-button的使用 -->
+      <div style="margin-bottom: 30px;">
+          <el-button>Default</el-button>
+          <el-button @click="click" type="success" plain  >Success</el-button>
+          <el-button @click="clear" type="info">Clear</el-button>
+      </div>
+  
+  
+      <div style="margin-bottom: 30px;">
+          <el-icon :size = "20">
+              <Edit />
+          </el-icon>
+  
+  
+          <span style="margin-left: 10px; color: red;">
+              <el-icon :size = "20" style="top: 4px;"><View /></el-icon> 100
+          </span>
+  
+          <el-button type="primary">
+              <el-icon style="vertical-align: middle">
+              <Delete />
+              </el-icon>
+              <span style="vertical-align: middle"> Delete </span> 
+          </el-button >
+  
+          <el-button type="primary">
+              <el-icon style="vertical-align: middle">
+              <Search />
+              </el-icon>
+              <span style="vertical-align: middle"> Search </span>
+          </el-button>
+      </div>
+  
+  
+      <div style="margin-bottom: 30px;">
+          <el-input
+      v-model="input"
+      style="width: 240px"
+      placeholder="Please input"
+      clearable/>
+      </div>
+  
+      <div style="margin-bottom: 30px;">
+          <el-input
+          v-model="input2"
+          style="width: 240px"
+          type="password"
+          placeholder="Please input password"
+          show-password
+          />
+      </div>
+  
+      <div style="margin-bottom: 30px;">
+          <el-input
+        v-model="input3"
+        style="width: 240px"
+        placeholder="请输入搜索内容"
+        :prefix-icon="Search"
+      />
+      </div>
+  
+  
+      <div style="margin-bottom: 30px;">
+          <el-button type="primary">提交</el-button>
+          <el-button type="primary" size="default" @click="">hello</el-button>
+          
+          
+      </div>
+  
+      <div style="margin-bottom: 30px;">
+          <el-input v-model="data.input" placeholder=""  clearable style="width: 300px;"></el-input>
+      </div>
+      
+    </div>
+  </template>
+  
+  
+  <script setup>
+  import { Edit, View, Delete, Search } from '@element-plus/icons-vue';
+  import { SignatureKind } from 'typescript';
+  import { reactive, ref } from 'vue';
+  // 两种方式声明变量
+  // import { ref } from 'vue'
+  const input = ref('')
+  const input2 = ref('')
+  const input3 = ref('')
+  const a = ref(1);
+  
+  const data = reactive    ({
+      name: '张三',
+      age: 20,
+      sex: '男',
+      arr: [1, 2, 3, 15],
+      fruits: ['apple', 'banana', 'orange'],
+      cnt : 0,
+      box : {
+          color: 'red',
+          fontSize : '16px',
+          backgroundColor : 'blue',
+          width : '100px',
+          height : '100px',
+          lineHeight : '100px',
+          textAlign : 'center',
+          
+  
+      },
+  
+      img : 'https://www.baidu.com/img/flexible/logo/pc/result.png',
+      input : '',
+  });
+  
+  const click = () => {
+      data.cnt++;
+  }
+  
+  const show = (value) => {
+      alert(value)
+  }
+  
+  const clear = () => {
+      data.cnt = 0;
+  }
+  </script>
+  
